8531053ded78af1e575b9b86893fa3227227d978
Ankit Josh <ankitjosh78@protonmail.com>
2021-08-06 12:39:39 +0530

    Segment Tree modifications

--- structures/segment_tree.cpp
+++ structures/segment_tree.cpp
@@ -15,26 +15,28 @@ void build(int node,int left,int right){
     int mid=(left+right)/2;
     build(2*node,left,mid);
     build(2*node+1,mid+1,right);
-    tree[node]=tree[2*node] + tree[2*node +1];
+    tree[node]=tree[2*node] + tree[2*node +1]; // change line as per question
 }
  
 // calculating sum for a given range
 long long sum(int node,int left_query,int right_query,int left_bound,int right_bound){
     if(right_query < left_bound || left_query > right_bound)
-        return 0;
+        return 0;  // change line as per question
     if(left_bound >= left_query && right_query >= right_bound)
         return tree[node];
     int mid=(right_bound + left_bound)/2;
     
-    return sum(2*node,left_query,right_query,left_bound,mid) + sum(2*node+1,left_query,right_query,mid+1,right_bound);
+    return sum(2*node,left_query,right_query,left_bound,mid)+sum(2*node+1,left_query,right_query,mid+1,right_bound) ;// change line as per question
 }
  
 // updating value at a given position and the segment tree
 void update(int index,int value,int n){
-    int difference=value - tree[n+index-1];
     tree[n+index-1]=value;
     for(int i=n+index-1;i>=1;i/=2){
-        tree[i/2]+=difference;
+        if(i & 1)
+            tree[i/2]=tree[i] + tree[i-1];
+        else 
+            tree[i/2]=tree[i] + tree[i+1];
     }
 }
 
